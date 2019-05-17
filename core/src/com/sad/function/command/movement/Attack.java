4056a4537d466693b419a78ed3bbbe4df2317399
Dragonegger2 <trent.lennon@gmail.com>
2019-05-17 18:51:35 -0400

    Removed the badlogic.jpg from the atlas. Created my own NULL texture.

---
+++ core/src/com/sad/function/command/movement/Attack.java
@@ -0,0 +1,13 @@
+package com.sad.function.command.movement;
+
+import com.artemis.World;
+import com.sad.function.command.GameCommand;
+
+public class Attack implements GameCommand {
+
+    @Override
+    public void execute(World world, int entity, float delta) {
+        //Set the animation to attack!
+    }
+
+}
