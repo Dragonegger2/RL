2d2e369bf7c37f938ab089228c1d7a27e90b18d8
Dragonegger2 <trent.lennon@gmail.com>
2019-06-13 19:03:18 -0400

    S

--- desktop/src/com/sad/function/game/desktop/DesktopLauncher.java
+++ desktop/src/com/sad/function/game/desktop/DesktopLauncher.java
@@ -11,8 +11,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.forceExit = true;
-//		new LwjglApplication(new ApocalypticGame(), config);
-		new LwjglApplication(new ShapeTest2(), config);
+		new LwjglApplication(new ApocalypticGame(), config);
+//		new LwjglApplication(new ShapeTest2(), config);
 //		new LwjglApplication(new ShapeTest(), config);
 	}
 }
